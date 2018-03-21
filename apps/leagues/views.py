05b5b63eb39942bf1a0c18ca9e7f19ff14581422
Wesley Cowan <wncowan@gmail.com>
2018-03-20 19:12:43 -0500

    first commit

--- apps/leagues/views.py
+++ apps/leagues/views.py
@@ -5,9 +5,9 @@ from . import team_maker
 
 def index(request):
 	context = {
-		"leagues": League.objects.all(),
-		"teams": Team.objects.raw("SELECT * FROM leagues_Team WHERE location = 'Dallas'"),
-		"players": Player.objects.filter(first_name="Alexander")|Player.objects.filter(first_name="Wyatt")
+		"leagues": League.objects.filter(teams__curr_players__first_name="Sophia"),
+		"teams": Team.objects.filter(all_players__first_name="Jacob", all_players__last_name="Gray").exclude(curr_players__first_name="Jacob", curr_players__last_name="Gray"),
+		"players": Player.objects.filter(all_teams__location="Wichita").filter(all_teams__team_name="Vikings").exclude(curr_team__team_name="Vikings")
 		}	
 	return render(request, "leagues/index.html", context)
 
